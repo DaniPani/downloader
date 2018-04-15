1452f2417c1fcf7f3bc700b5916d6333eeec8470
Daniel <panero.daniel@gmail.com>
2018-04-15 16:56:20 +0200

    Gitignore lines count, now downloader need the number of episode but later we can fix this (to improve performance), export loader in new files, rewrited ilgeniodellostreaming to improve perfomance and readbility

--- bin/downloader.js
+++ bin/downloader.js
@@ -2,8 +2,8 @@ const program = require('commander')
 const downloader = require('../')
 
 program
-  .command('download <url>')
+  .command('download <url> <times>')
   .description('Download all video in series')
-  .action(url => downloader(url))
+  .action((url, times) => downloader(url, times))
 
 program.parse(process.argv)
--- src/ilgeniodellostreaming.js
+++ src/ilgeniodellostreaming.js
@@ -1,33 +1,53 @@
 const puppeteer = require('puppeteer')
-module.exports = async function (url) {
-  let links = []
+
+module.exports = async function (url, times) {
+  let first = ''
   let info = url.replace(/http:\/\/ilgeniodellostreaming\.org\/episodi\//g, '').replace(/\//g, '').split(/-/g)
   let [season,, episode] = info.pop()
   season = parseInt(season)
   episode = parseInt(episode)
+  let promises = []
 
-  const browser = await puppeteer.launch({headless: false})
-  const page = await browser.newPage()
-  await page.setRequestInterception(true)
-  page.on('request', request => {
-    if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
-      request.abort()
-    } else {
-      request.continue()
-    }
-  })
-  await page.goto(url)
-  console.log('Loading:')
-  while (!await page.$('.error404')) {
-    console.log(`Season: ${season} Episode: ${episode}`)
-    await page.waitFor('.metaframe.rptss')
-    let src = await page.$eval('.metaframe.rptss', el => el.src)
-    links.push(src.replace('embed', 'f'))
-    episode = episode + 1
-    await page.goto(`http://ilgeniodellostreaming.org/episodi/${info.join('-')}-${season}x${episode}/`, {waitUntil: 'domcontentloaded'})
+  const browser = await puppeteer.launch()
+
+  const escapeAntiBot = async () => {
+    await browser.newPage().then(async page => {
+      await page.goto('http://ilgeniodellostreaming.org')
+      console.log('Escaping CloudFlare Protection AntiBOT...')
+      await page.waitFor('.hbox')
+      await page.close()
+    })
   }
-  await browser.close()
 
-  let name = info.join(' ')
-  return {name, links}
+  await escapeAntiBot()
+
+  const search = async (ep) => {
+    return browser.newPage().then(async page => {
+      await page.setRequestInterception(true)
+      page.on('request', request => {
+        if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
+          request.abort()
+        } else {
+          request.continue()
+        }
+      })
+      await page.goto(`http://ilgeniodellostreaming.org/episodi/${info.join('-')}-${season}x${ep}/`, {waitUntil: 'domcontentloaded'})
+      console.log(`Season: ${season} Episode: ${ep}`)
+      await page.waitFor('.metaframe.rptss')
+      const src = await page.$eval('.metaframe.rptss', el => el.src)
+      await page.close()
+      return src.replace('embed', 'f')
+    })
+  }
+  for (let index = 0; index < times; index++) {
+    promises.push(search(episode))
+    episode += 1
+  }
+
+  return Promise.all(promises).then(result => {
+    console.log(result)
+    browser.close()
+    let name = info.join(' ')
+    return {name, 'links': [first, ...result]}
+  })
 }
