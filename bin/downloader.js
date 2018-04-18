fe025218e17285c0d72df6ee0dd7907d7f2875eb
Daniel <panero.daniel@gmail.com>
2018-04-18 20:44:34 +0200

    Removed console commander

--- bin/downloader.js
+++
@@ -1,9 +0,0 @@
-const program = require('commander')
-const downloader = require('../')
-
-program
-  .command('download <url> <times>')
-  .description('Download all video in series')
-  .action((url, times) => downloader(url, times))
-
-program.parse(process.argv)
